e31f9122584f282105a5e20a022c16693d26221f
Ethan Kam <ethan@sgnl.ai>
2023-05-01 12:57:10 -0700

    Add receiver config struct

---
+++ pkg/receiver_config.go
@@ -0,0 +1,38 @@
+package pkg
+
+type ReceiverConfig struct {
+	// TransmitterUrl defines the URL for the transmitter that
+	// the configured receiver will create a stream with and receive
+	// events from.
+	//
+	// Required
+	TransmitterUrl string
+
+	// EventsRequested specified the CAEP events you want to receiver
+	// from the transmitter.
+	//
+	// Required
+	EventsRequested []EventNums
+
+	// AuthorizationToken is the authorization token used to authorize
+	// your receiver with the specified transmitter
+	// Note - all transmitter's will require an authorization token
+	//
+	// Required
+	AuthorizationToken string
+
+	// PushCallback is used to configure the method that you want the
+	// receiver to call when it's received CAEP events. Each time the
+	// receiver runs the push method, it will call the PushCallback and
+	// pass as a parameter the list of received CAEP events since the
+	// last push call.
+	//
+	// Optional
+	PushCallback func(events []CaepEvent)
+
+	// PushInterval defines, in seconds how often you want the receiver to
+	// push any CAEP events to your callback function.
+	//
+	// Optional, defaults to __
+	PushInterval int
+}
